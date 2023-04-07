e550bf53f56e74658d9dca4a6a9c72c100970aeb
Brian Inca <brianincah10@gmail.com>
2023-04-07 17:14:34 -0500

    Ordenando el código

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Arreglo con las respuestas correctas
-let r_correctas = [2,3,3,3,5,5];
+let r_correctas = [2, 3, 3, 3, 5, 5];
 // Arreglo con las respuestas del usuario
 let r_usuario = [];
 
@@ -7,7 +7,7 @@ let r_usuario = [];
 function respuesta(n_pregunta, respuesta) {
 
     //Guardar las respuestas del usuario
-    r_usuario[n_pregunta-1] = respuesta.value;
+    r_usuario[n_pregunta - 1] = respuesta.value;
 
     //Saber el id del div contenedor de la pregunta
     id = "p" + n_pregunta;
@@ -30,7 +30,7 @@ function respuesta(n_pregunta, respuesta) {
         labels[7].style.backgroundColor = "#fff";
         labels[9].style.backgroundColor = "#fff";
         labels[11].style.backgroundColor = "#fff";
-        
+
         labels[3].style.fontWeight = "400";
         labels[5].style.fontWeight = "400";
         labels[7].style.fontWeight = "400";
@@ -40,18 +40,18 @@ function respuesta(n_pregunta, respuesta) {
 
     //Poner el fondo amarillo de la respuesta elegida
     respuesta.parentNode.style.backgroundColor = "rgb(243, 243, 31)";
-    respuesta.parentNode.style.fontWeight= "700";
+    respuesta.parentNode.style.fontWeight = "700";
 }
 
 function corregir() {
-    
+
     // Variable de cantidad de respuestas correctas
     let cnt_r_correctas = 0;
 
     for (let i = 0; i < r_correctas.length; i++) {
         if (r_correctas[i] == r_usuario[i]) {
             cnt_r_correctas++;
-        }   
+        }
     }
     document.getElementById("resultado").innerHTML = cnt_r_correctas;
 }
